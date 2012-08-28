84317aefa32c2960e1fc3368c7c3e77c90e5b72b
Chris Hasenpflug <chris@hasenpflug.net>
2012-08-27 22:50:29 -0500

    Initial commit

---
+++ sentry.conf.py
@@ -0,0 +1,67 @@
+import os.path
+import sys
+
+import dj_database_url
+
+
+CONF_ROOT = os.path.dirname(__file__)
+
+try:
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'GONDOR_DATABASE_URL' in os.environ:
+        DATABASES['default'] = dj_database_url.config(env='GONDOR_DATABASE_URL')
+    elif 'DATABASE_URL' in os.environ:
+        DATABASES['default'] = dj_database_url.config(env='DATABASE_URL')
+except Exception:
+    print 'Unexpected error:', sys.exc_info()
+
+SENTRY_KEY = os.environ['SENTRY_KEY']
+
+# Set this to false to require authentication
+SENTRY_PUBLIC = False
+
+# You should configure the absolute URI to Sentry. It will attempt to guess it if you don't
+# but proxies may interfere with this.
+# SENTRY_URL_PREFIX = 'http://sentry.example.com'  # No trailing slash!
+
+SENTRY_WEB_HOST = '0.0.0.0'
+SENTRY_WEB_PORT = 9000
+SENTRY_WEB_OPTIONS = {
+    'workers': 3,  # the number of gunicorn workers
+    # 'worker_class': 'gevent',
+}
+
+# Mail server configuration
+
+# For more information check Django's documentation:
+#  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ''
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+
+# http://twitter.com/apps/new
+# It's important that input a callback URL, even if its useless. We have no idea why, consult Twitter.
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+
+# http://developers.facebook.com/setup/
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_SECRET = ''
+
+# http://code.google.com/apis/accounts/docs/OAuth2.html#Registering
+GOOGLE_OAUTH2_CLIENT_ID = ''
+GOOGLE_OAUTH2_CLIENT_SECRET = ''
+
+# https://github.com/settings/applications/new
+GITHUB_APP_ID = ''
+GITHUB_API_SECRET = ''
